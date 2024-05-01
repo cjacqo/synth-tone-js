890c80feb6d8d5d0ec47850f097b93bc847670c7
cjacqo <76092910+cjacqo@users.noreply.github.com>
2024-05-01 13:48:23 -0700

    Had to declare a module for audiokeys

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import SynthProvider from './components/synth/Synth'
 import FXChain from './components/fx/FXChain'
+import Keyboard from './components/keyboard/Keyboard'
 
 function App() {
   return (
     <>
       <SynthProvider>
         <FXChain />
+        <Keyboard />
       </SynthProvider>
     </>
   )
